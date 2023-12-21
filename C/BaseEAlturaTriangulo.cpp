0e9ab7c1aa43ccd3dcad2a982f16050890aecd78
Rafael dos Santos Giorgi <106035150+rafaelnator@users.noreply.github.com>
2023-12-21 14:08:20 -0300

    Update BaseEAlturaTriangulo.cpp

--- C/BaseEAlturaTriangulo.cpp
+++ C/BaseEAlturaTriangulo.cpp
@@ -1,7 +1,7 @@
 /* ==========================================================================
 Implemente um algoritmo em linguagem C que leia a base e a altura de um triangulo
-e calcule a sua área. 
-Obs.: a base a altura desses triangulos podem ser números decimais. 
+e calcule a sua Ã¡rea. 
+Obs.: a base a altura desses triangulos podem ser nÃºmeros decimais. 
 ============================================================================*/
 
 #include <stdio.h>
@@ -9,13 +9,19 @@ int main (){
 	
 	float n1, n2, n3, n4, resultado = 0.00;
 	printf("digite Altura:");
+	fflush(stdout);
 	scanf("%f",&n1);
+	fflush(stdin);
 	
 	printf("digite Base:");
+	fflush(stdout);
 	scanf("%f",&n2);
+	fflush(stdin);
 	
 	resultado = (n1 * n2)/2;
 	
 	printf("Area:%.2f",resultado);
+	fflush(stdout);
+	
 	return 0;
 }
