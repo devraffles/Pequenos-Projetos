baf54c3c24488c201faec4b070828ca9d6500115
Rafael dos Santos Giorgi <106035150+rafaelnator@users.noreply.github.com>
2023-12-21 14:12:21 -0300

    Update Dividereais.cpp

--- C/Dividereais.cpp
+++ C/Dividereais.cpp
@@ -5,10 +5,14 @@ int main (){
 	
 	float n1, n2, resultado = 0;
 	printf("digite primeiro numero:");
+	fflush(stdout);
 	scanf("%f",&n1);
+	fflush(stdin);
 	
 	printf("digite segundo numero:");
+	fflush(stdout);
 	scanf("%f",&n2);
+	fflush(stdin);
 	
 	resultado = n1 / n2;
 	
