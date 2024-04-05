e9acc4a19329ceed6e1477a824d7b942ebc36727
Rafael dos Santos Giorgi <106035150+rafaelnator@users.noreply.github.com>
2024-04-05 00:36:26 -0300

    Update Cinema.cpp

--- C/Unimetrocamp/Cinema.cpp
+++ C/Unimetrocamp/Cinema.cpp
@@ -9,76 +9,88 @@ typedef struct{
 
 typedef struct{
     char sala[50];
-    char poutrona[3]
+    char poutrona[3];
 } Cinema;
+
+typedef struct{
+    char nome[50];
+    int etaria[3];
+} Filme;
 
 typedef struct{
     char tipo[50];
     float preco;
     Cliente cli;
     Cinema cine;
-    char nome_Filme[50];
-    
+    Filme movie[4];
 } Ticket;
 
 int main() {
-    Cinema c;
     Ticket t;
     
+    
+    strcpy(t.movie[1].nome, "Homem Aranha");
+    strcpy(t.movie[2].nome, "Duna");
+    strcpy(t.movie[3].nome, "Maze Runner");
+    
+    t.movie[1].etaria[1] = 18;
+    t.movie[2].etaria[2] = 14;
+    t.movie[3].etaria[3] = 12;
+    
     int op, i = 0;
+    
     
     printf("---------------------------------------\n");
     printf("|         BEM-VINDO CINEJORGES        |\n");
     printf("---------------------------------------\n");
     
-    for(i = 0; i <=3; i++){
-        printf("\nNome do cliente: ");
-        scanf("%s", c.sala.cliente[i].nome);
+    
+    printf("\nNome do cliente: ");
+    scanf("%s", t.cli.nome);
         
-        printf("\nQual a sua idade %s: ", c.nome);
-        scanf("%d", &c.sala.cliente[i].idade);
+    printf("\nQual a sua idade %s: ", t.cli.nome);
+    scanf("%d", &t.cli.idade);
         
-        if(c.sala.cliente[i].idade <= 18){
-            strcpy(t.tipo, "MEIA");
-            t.preco = 5.90;
-        } else{
-            strcpy(t.tipo, "INTEIRA");
-            t.preco = 11.90;
-        }
+    if(t.cli.idade <= 18){
+        strcpy(t.tipo, "MEIA");
+        t.preco = 5.90;
+    } else{
+        strcpy(t.tipo, "INTEIRA");
+        t.preco = 11.90;
     }
     
     printf(" -------------------------------------------------------------\n");
     printf(" |                         Filmes                            |\n");
     printf(" -------------------------------------------------------------\n");
-    printf(" |(1) - Homem Aranha Facha Etaria (18 anos)                  |\n");
+    printf(" |(3) - %s Facha Etaria (%d anos)                  |\n", t.movie[1].nome, t.movie[1].etaria[1]);
     printf(" -------------------------------------------------------------\n");
-    printf(" |(2) - Duna  Facha Etaria (14 anos)                         |\n");
+    printf(" |(3) - %s Facha Etaria (%d anos)                          |\n", t.movie[2].nome, t.movie[2].etaria[2]);
     printf(" -------------------------------------------------------------\n");
-    printf(" |(3) - Maze Runner Facha Etaria (12 anos)                   |\n");
+    printf(" |(3) - %s Facha Etaria (%d anos)                   |\n", t.movie[3].nome, t.movie[3].etaria[3]);
     printf(" -------------------------------------------------------------\n");
     printf("\nDigite o filme desejada: ");
     scanf("%d",&op);
     
     switch (op){
         case 1:
-            if(c.idade < 18){
+            if(t.cli.idade < t.movie[1].etaria[1]){
                 printf("Voce nao pode assistir");
             } else{
-                printf("O preco desse ingresso é %s", t.tipo);
+                printf("O seu ingresso é %s e o preco é %f", t.tipo, t.preco);
             }
             break;
         case 2:
-            if(c.idade < 14){
+            if(t.cli.idade < t.movie[2].etaria[2]){
                 printf("Voce nao pode assistir");
             } else{
-                printf("O preco desse ingresso é %s", t.tipo);
+                printf("O seu ingresso é %s e o preco é %f", t.tipo, t.preco);
             }
             break;
         case 3:
-            if(c.idade < 14){
+            if(t.cli.idade < t.movie[3].etaria[3]){
                 printf("Voce nao pode assistir");
             } else{
-                printf("O preco desse ingresso é %s", t.tipo);
+                printf("O seu ingresso é %s e o preco é %f", t.tipo, t.preco);
             }
             break;
         default:
