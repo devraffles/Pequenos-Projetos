5476e5ddf1d7ad1ba3bac2a4ce8d94f1140ce724
Rafael dos Santos Giorgi <106035150+rafaelnator@users.noreply.github.com>
2024-05-23 20:41:11 -0300

    Atualizar o Estudo_matriz.cpp

--- C/Unimetrocamp/Estudo_matriz.cpp
+++ C/Unimetrocamp/Estudo_matriz.cpp
@@ -20,7 +20,7 @@ int main() {
     printf("\n------------------\n");
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
-            printf("| %d |", m[i][j]);
+            printf("[ %d ]", m[i][j]);
         }
         printf("\n");
     }
