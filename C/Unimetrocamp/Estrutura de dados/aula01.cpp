80fe9e5da2c5a0506811d8b3e3262d7c1b18be0c
devraffles <devraffles@gmail.com>
2024-09-05 21:43:20 -0300

    Jogo

--- C/Unimetrocamp/Estrutura de dados/aula01.cpp
+++
@@ -1,3 +0,0 @@
-// se um pointer do tipo int for incrementado (pointer++) ele ira avançar para o próximo int
-// agora se um pointer for de um struct ele vai avançar para o próximo struct, porém na memóriamele ira avançar
-// a soma dos tamanhos de de cada varáviel
---
+++ Python/Unimetrocamp/aula04.py
@@ -0,0 +1,48 @@
+class Player:
+    def __init__(self, nome):
+        self.nome = nome
+
+    x = 0
+    y = 0
+
+    def getNome(self):
+        return self.nome
+    
+    def setNome(self, nome):
+        self.nome = nome
+
+    def getX(self):
+        return self.nome
+    
+    def setX(self, x):
+        self.x = x
+    
+    def getY(self):
+        return self.y
+    
+    def setY(self, y):
+        self.y = y
+
+    def Pular(self):
+        self.y += 1
+        print(f"Player {self.getNome()} pulou: {(self.x, self.y)}")
+
+    def Agachar(self):
+        self.y -= 1
+        print(f"Player {self.getNome()} agachou: {(self.x, self.y)}")
+
+    def andarEsquerda(self):
+        self.x -= 1
+        print(f"Player {self.getNome()} andou para esquerda: {(self.x, self.y)}")
+
+    def andarDireita(self):
+        self.x += 1
+        print(f"Player {self.getNome()} andou para direita: {(self.x, self.y)}")
+
+p = Player("Rafael")
+p2 = Player("Pele")
+
+p.Agachar()
+p.Pular()
+p.andarDireita()
+p.andarEsquerda()
